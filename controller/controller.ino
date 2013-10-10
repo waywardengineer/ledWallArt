0b6d6410e05de14545265d018941d1ebb53e2d70
ray <waywardengineer@gmail.com>
2013-10-09 19:17:25 -0700

    work in progress, wont work yet

--- controller/controller.ino
+++ controller/controller.ino
@@ -5,8 +5,31 @@
 #define PCA9685_CONFIG_BYTE1 0b00100001
 #define PCA9685_CONFIG_BYTE2 0b00011001
 
-const byte sinCurve255[256] = {128, 131, 134, 137, 140, 143, 146, 149, 153, 156, 159, 162, 165, 168, 171, 174, 177, 180, 182, 185, 188, 191, 194, 196, 199, 201, 204, 207, 209, 211, 214, 216, 218, 220, 223, 225, 227, 229, 231, 232, 234, 236, 238, 239, 241, 242, 243, 245, 246, 247, 248, 249, 250, 251, 252, 253, 253, 254, 254, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 254, 254, 253, 253, 252, 251, 251, 250, 249, 248, 247, 245, 244, 243, 241, 240, 238, 237, 235, 233, 232, 230, 228, 226, 224, 222, 219, 217, 215, 213, 210, 208, 205, 203, 200, 198, 195, 192, 189, 187, 184, 181, 178, 175, 172, 169, 166, 163, 160, 157, 154, 151, 148, 145, 142, 139, 135, 132, 129, 127, 124, 121, 117, 114, 111, 108, 105, 102, 99, 96, 93, 90, 87, 84, 81, 78, 75, 72, 69, 67, 64, 61, 58, 56, 53, 51, 48, 46, 43, 41, 39, 37, 34, 32, 30, 28, 26, 24, 23, 21, 19, 18, 16, 15, 13, 12, 11, 9, 8, 7, 6, 5, 5, 4, 3, 3, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 17, 18, 20, 22, 24, 25, 27, 29, 31, 33, 36, 38, 40, 42, 45, 47, 49, 52, 55, 57, 60, 62, 65, 68, 71, 74, 76, 79, 82, 85, 88, 91, 94, 97, 100, 103, 107, 110, 113, 116, 119, 122, 125};
-const uint16_t sinCurve360[256] = {180, 184, 188, 193, 197, 202, 206, 210, 215, 219, 223, 228, 232, 236, 240, 244, 248, 252, 256, 260, 264, 268, 272, 276, 280, 283, 287, 290, 294, 297, 300, 304, 307, 310, 313, 316, 319, 321, 324, 327, 329, 332, 334, 336, 338, 340, 342, 344, 346, 347, 349, 350, 352, 353, 354, 355, 356, 357, 358, 358, 359, 359, 359, 359, 360, 359, 359, 359, 359, 358, 358, 357, 356, 355, 354, 353, 352, 350, 349, 347, 346, 344, 342, 340, 338, 336, 334, 332, 329, 327, 324, 321, 319, 316, 313, 310, 307, 304, 300, 297, 294, 290, 287, 283, 280, 276, 272, 268, 264, 260, 256, 252, 248, 244, 240, 236, 232, 228, 223, 219, 215, 210, 206, 202, 197, 193, 188, 184, 180, 176, 172, 167, 163, 158, 154, 150, 145, 141, 137, 132, 128, 124, 120, 116, 112, 108, 104, 100, 96, 92, 88, 84, 80, 77, 73, 70, 66, 63, 60, 56, 53, 50, 47, 44, 41, 39, 36, 33, 31, 28, 26, 24, 22, 20, 18, 16, 14, 13, 11, 10, 8, 7, 6, 5, 4, 3, 2, 2, 1, 1, 1, 1, 0, 1, 1, 1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 10, 11, 13, 14, 16, 18, 20, 22, 24, 26, 28, 31, 33, 36, 39, 41, 44, 47, 50, 53, 56, 60, 63, 66, 70, 73, 77, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116, 120, 124, 128, 132, 137, 141, 145, 150, 154, 158, 163, 167, 172, 176};
+
+#define NUM_ARMS 3
+#define NUM_LEDS_PER_ARM 10
+#define KEYFRAMESPACING 2000
+#define NUM_PATTERN_LAYERS 1
+
+const uint8_t ledOrdering[NUM_LEDS_PER_ARM] = {0, 4, 1, 2, 3, 5, 9, 6, 8, 7};
+typedef struct {
+	uint8_t strengthFactor; //relative intensity, 0-90
+	uint8_t timebase; //length for cycle
+	uint8_t phaseOffset;
+	uint8_t positionVariance;
+	//uint8_t blending; // 0=no blending over space, 1=blending over space
+	uint16_t ledMask; //mask for which leds are affected by pattern
+	uint8_t armMask;
+	uint8_t currentTime;
+	uint16_t hues[2];
+	uint8_t saturations[2];
+	uint8_t brightnesses[2];
+} SinePatternLayer;
+SinePatternLayer sinePatternLayers[NUM_PATTERN_LAYERS];
+uint16_t frames[3][NUM_ARMS][NUM_LEDS_PER_ARM][3];
+unsigned long keyFrameExpiration;
+uint16_t rgb[3];
+
 byte rPin = 14;
 byte gPin = 15;
 byte bPin = 16;
@@ -22,6 +45,19 @@ void setup(){
 	setParams(0);
 	setParams(1);
 	Serial.begin(9600);
+	sinePatternLayers[0].strengthFactor = 90;
+	sinePatternLayers[0].timebase = 30;
+	sinePatternLayers[0].phaseOffset = 0;
+	sinePatternLayers[0].positionVariance = 10;
+	sinePatternLayers[0].ledMask = 0b0000001111111111;
+	sinePatternLayers[0].hues[0] = 290;
+	sinePatternLayers[0].hues[1] = 320;
+	sinePatternLayers[0].saturations[0] = 200;
+	sinePatternLayers[0].saturations[1] = 200;
+	sinePatternLayers[0].brightnesses[0] = 255;
+	sinePatternLayers[0].brightnesses[1] = 200;
+	calculateNextKeyFrame();
+	calculateNextKeyFrame();
 }
 void loop(){
 	
@@ -68,7 +104,6 @@ void sendByte(uint8_t chipAddr, uint8_t regAddr, uint8_t data) {
 
 
 void writeLedColor(uint8_t arm, uint8_t ledIndex, uint16_t hsv[3])  { //hsv[0] hue, 0-360; hsv[1], 0-255; hsv[2], 0-255
-	uint16_t rgb[3];
 	uint8_t chipAddr = arm * 2;
 	if (ledIndex > 4){
 		chipAddr++;
@@ -104,7 +139,49 @@ void writeLedColor(uint8_t arm, uint8_t ledIndex, uint16_t hsv[3])  { //hsv[0] h
 		Serial.println("======");
 	}*/
 }
-const uint16_t dim_curve[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 26, 27, 28, 30, 31, 32, 34, 35, 37, 38, 40, 41, 43, 44, 46, 48, 49, 51, 53, 55, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 79, 81, 83, 85, 88, 90, 93, 95, 98, 100, 103, 106, 108, 111, 114, 117, 120, 122, 125, 129, 132, 135, 138, 141, 145, 148, 152, 155, 159, 162, 166, 170, 174, 177, 181, 185, 190, 194, 198, 202, 207, 211, 216, 220, 225, 230, 235, 240, 245, 250, 255, 261, 266, 272, 277, 283, 289, 295, 301, 307, 313, 319, 326, 333, 339, 346, 353, 360, 367, 375, 382, 390, 397, 405, 413, 421, 429, 438, 446, 455, 464, 473, 482, 491, 501, 510, 520, 530, 540, 551, 561, 572, 583, 594, 605, 617, 629, 640, 653, 665, 677, 690, 703, 716, 730, 743, 757, 772, 786, 801, 816, 831, 846, 862, 878, 894, 911, 928, 945, 962, 980, 998, 1016, 1035, 1054, 1074, 1093, 1113, 1134, 1154, 1176, 1197, 1219, 1241, 1264, 1287, 1310, 1334, 1359, 1383, 1409, 1434, 1460, 1487, 1514, 1541, 1569, 1598, 1626, 1656, 1686, 1716, 1747, 1779, 1811, 1844, 1877, 1911, 1945, 1980, 2016, 2052, 2089, 2127, 2165, 2204, 2243, 2284, 2325, 2366, 2409, 2452, 2496, 2540, 2586, 2632, 2679, 2727, 2776, 2826, 2876, 2928, 2980, 3033, 3087, 3142, 3198, 3255, 3314, 3373, 3433, 3494, 3556, 3619, 3684, 3750, 3816, 3884, 3953, 4024, 4095};
+void calculateNextKeyFrame(){
+	shiftKeyFrames();
+	for (ledIndex=0; ledIndex < NUM_LEDS_PER_ARM; ledIndex++){
+		uint16_t hsv[NUM_ARMS][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+		for (patternIndex=0; patternIndex < NUM_PATTERN_LAYERS; patternIndex++){
+			if (sinePatternLayers[patternIndex].strengthFactor > 0 && (sinePatternLayers[patternIndex].ledMask & (1 << ledIndex))){
+				uint16_t interval = (sinePatternLayers[patternIndex].currentTime + ledIndex * sinePatternLayers[patternIndex].positionVariance + sinePatternLayers[patternIndex].phaseOffset) % 255;
+				uint16_t thisHsv[3] = {
+					calculateSin(sinePatternLayers[patternIndex].hues[0], sinePatternLayers[patternIndex].hues[1], interval),
+					calculateSin(sinePatternLayers[patternIndex].saturations[0], sinePatternLayers[patternIndex].saturations[1], interval),
+					calculateSin(sinePatternLayers[patternIndex].brightnesses[0], sinePatternLayers[patternIndex].brightnesses[1], interval)
+				};
+				for (uint8_t armIndex = 0; armIndex < NUM_ARMS; armIndex++){
+					if (sinePatternLayers[patternIndex].armMask & (1<<armIndex)){
+						for (uint8_t colorIndex = 0; colorIndex < 3; colorIndex++){
+							if (patternIndex == 0){
+								hsv[armIndex][colorIndex] = thisHsv[colorIndex];
+							}
+							else{
+								uint16_t val = (hsv[armIndex][colorIndex]*90 + thisHsv[colorIndex]*sinePatternLayers[patternIndex].strengthFactor)/180;
+								hsv[armIndex][colorIndex] = val;
+							}
+						}
+					}
+				}
+			}
+		}
+		for (uint8_t armIndex = 0; armIndex < NUM_ARMS; armIndex++){
+			getRGB(hsv[armIndex][0], hsv[armIndex][1], hsv[armIndex][2], rgb);
+			for (uint8_t colorIndex = 0; colorIndex < 3; colorIndex++){
+				frames[2][armIndex][ledIndex][colorIndex] = rgb[colorIndex];
+			}
+		}
+	}
+}
+
+const uint16_t dim_curve[256] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 26, 27, 28, 30, 31, 32, 34, 35, 37, 38, 40, 41, 43, 44, 46, 48, 49, 51, 53, 55, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 79, 81, 83, 85, 88, 90, 93, 95, 98, 100, 103, 106, 108, 111, 114, 117, 120, 122, 125, 129, 132, 135, 138, 141, 145, 148, 152, 155, 159, 162, 166, 170, 174, 177, 181, 185, 190, 194, 198, 202, 207, 211, 216, 220, 225, 230, 235, 240, 245, 250, 255, 261, 266, 272, 277, 283, 289, 295, 301, 307, 313, 319, 326, 333, 339, 346, 353, 360, 367, 375, 382, 390, 397, 405, 413, 421, 429, 438, 446, 455, 464, 473, 482, 491, 501, 510, 520, 530, 540, 551, 561, 572, 583, 594, 605, 617, 629, 640, 653, 665, 677, 690, 703, 716, 730, 743, 757, 772, 786, 801, 816, 831, 846, 862, 878, 894, 911, 928, 945, 962, 980, 998, 1016, 1035, 1054, 1074, 1093, 1113, 1134, 1154, 1176, 1197, 1219, 1241, 1264, 1287, 1310, 1334, 1359, 1383, 1409, 1434, 1460, 1487, 1514, 1541, 1569, 1598, 1626, 1656, 1686, 1716, 1747, 1779, 1811, 1844, 1877, 1911, 1945, 1980, 2016, 2052, 2089, 2127, 2165, 2204, 2243, 2284, 2325, 2366, 2409, 2452, 2496, 2540, 2586, 2632, 2679, 2727, 2776, 2826, 2876, 2928, 2980, 3033, 3087, 3142, 3198, 3255, 3314, 3373, 3433, 3494, 3556, 3619, 3684, 3750, 3816, 3884, 3953, 4024, 4095};
+const uint8_t sinCurve255[256] = {128, 131, 134, 137, 140, 143, 146, 149, 153, 156, 159, 162, 165, 168, 171, 174, 177, 180, 182, 185, 188, 191, 194, 196, 199, 201, 204, 207, 209, 211, 214, 216, 218, 220, 223, 225, 227, 229, 231, 232, 234, 236, 238, 239, 241, 242, 243, 245, 246, 247, 248, 249, 250, 251, 252, 253, 253, 254, 254, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 254, 254, 253, 253, 252, 251, 251, 250, 249, 248, 247, 245, 244, 243, 241, 240, 238, 237, 235, 233, 232, 230, 228, 226, 224, 222, 219, 217, 215, 213, 210, 208, 205, 203, 200, 198, 195, 192, 189, 187, 184, 181, 178, 175, 172, 169, 166, 163, 160, 157, 154, 151, 148, 145, 142, 139, 135, 132, 129, 127, 124, 121, 117, 114, 111, 108, 105, 102, 99, 96, 93, 90, 87, 84, 81, 78, 75, 72, 69, 67, 64, 61, 58, 56, 53, 51, 48, 46, 43, 41, 39, 37, 34, 32, 30, 28, 26, 24, 23, 21, 19, 18, 16, 15, 13, 12, 11, 9, 8, 7, 6, 5, 5, 4, 3, 3, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 17, 18, 20, 22, 24, 25, 27, 29, 31, 33, 36, 38, 40, 42, 45, 47, 49, 52, 55, 57, 60, 62, 65, 68, 71, 74, 76, 79, 82, 85, 88, 91, 94, 97, 100, 103, 107, 110, 113, 116, 119, 122, 125};
+
+uint16_t calculateSin(uint16_t bottomValue, uint16_t topValue, uint8_t interval){
+	
+}
+
 
 void getRGB(uint16_t hue, uint16_t sat, uint16_t val, uint16_t colors[3]) { 
 
